75767cf16451dcb1467635852ee353062ad08b6c
Bhavdeep Dhanjal <bhavdeep@hey.com>
2023-02-07 00:42:54 +0100

    read from cmdline and fetch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::process;
+use reqwest;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let url = parse_url(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let client = reqwest::Client::new();
+    let res = client.get(&url);
+    println!("fetching url: {}", &url);
+}
+
+fn parse_url(args: &[String]) -> Result<String, &'static str> {
+    if args.len() < 2 {
+        return Err("please pass a url");
+    } else if args.len() > 2 {
+        return Err("please pass only one arg");
+    }
+
+    // TODO: manage references better later
+    let url = args[1].clone();
+
+    Ok(url)
 }
